84d56a7e3e7b6581eb214d3f53f3ed31bbaaa834
Frenz86 <frenzy86@gmail.com>
2020-12-21 06:02:18 +0100

    ww

--- pag1.py
+++ pag1.py
@@ -9,9 +9,9 @@ import folium
 from streamlit_folium import folium_static
 import matplotlib, matplotlib.pyplot as plt 
 import branca
-import shapefile #pip install pyshp
-import shapely.speedups
-shapely.speedups.enable()
+import shapefile #pip install pyshp#
+#import shapely.speedups
+#shapely.speedups.enable()
 
 
 def read_shapefile(shp_path):
@@ -80,69 +80,58 @@ def main():
 		latitude = float(lat)
 		longitude = float(longi)
 
-	#DATAFRAME from SHAPEFILE
-	# df1 = read_shapefile(landslide_path)
-	# df2 = read_shapefile(flood_path)
-	# print(df2)
-
 	if st.button('Analyse Lat & Long'): # this is if you want to add a button to launch the analysis (without this, it does automatically when there's lat & long values in the cell)
 		st.header('Extracting Results for the location selected:\n(Lat: ' + str(latitude) +' & Long: ' + str(longitude) + ')')
 		# ======= Get Value from Shapefile
-		landslide_code = 'Outside Risk Zone'
 
-		p = Point(longitude,latitude)
-		def risk_land(longitude,latitude):
-			for i in landslide_shp.loc[:,'geometry']:
-				if p.within(i): 
-					landslide_code = landslide_shp.loc[landslide_shp.loc[:,'geometry'] == i]['LANDSLIDES'].values[0]
-					return landslide_code	
-
-		risk_land(longitude,latitude)
-		st.markdown('**-Landslide Risk: **'+ landslide_code)
-		st.write('wait for Flood Risk Analysis... ')
+		## mycode
+		#coordinate = shapely.geometry.Point((-61.346482,15.393996,)) # outside
+		#coordinate = shapely.geometry.Point((-61.419855,15.396184,)) # 1771
+		coordinate = shapely.geometry.Point((longitude,latitude,))
+		# Printing a list of the coords to ensure iterable 
+		#list(coordinate.coords)
+		# Searching for the geometry that intersects the point. Returning the index for the appropriate polygon. 
+		for i in landslide_shp.loc[:,'geometry']:
+			p = Point(longitude,latitude)
+			if p.within(i):
+				polig_landslide = landslide_shp[landslide_shp.geometry.intersects(coordinate)].values[0][0]
+				landslide_code =polig_landslide
+				print('find_landslide_pol',landslide_code)
+				st.markdown('**-Landslide Risk: **'+ landslide_code)
+				st.write('wait for Flood Risk Analysis... ')
+				break
+		else:
+			landslide_code = 'Outside Risk Zone'
+			print('find_landslide_pol',landslide_code)
+			st.markdown('**-Landslide Risk: **'+ landslide_code)
 		
-##################à##		
-		# for i in landslide_shp.loc[:,'geometry']:
-		# 	if p.within(i): 
-		# 		landslide_code = landslide_shp.loc[landslide_shp.loc[:,'geometry'] == i]['LANDSLIDES'].values[0]
-		# st.markdown('**-Landslide Risk: **'+ landslide_code)
-		# st.write('wait for Flood Risk Analysis... ')
-
-		# ======= Get Value from Raster
-		# From a given Point coordinates, quickly/efficiently check if it's contained in any polygons geometry, and print out the LANDSLIDE code of that polygon if so. 
-		# this works, but is this teh most efficient way?!? Think it can be improved with lampda / apply / map, without iterating on the whole dataframe?
 		frisk_code = 'NAN'
 		new_risk = 'NAN'
-		for k in flood_shp.loc[:,'geometry']:
-			if p.within(k): 
-				frisk_code = flood_shp.loc[flood_shp.loc[:,'geometry'] == k]['FloodRisk'].values[0]
+		for i in flood_shp.loc[:,'geometry']:
+			p = Point(longitude,latitude)
+			if p.within(i):	
+				frisk_code = flood_shp[flood_shp.geometry.intersects(coordinate)].values[0][0]
 				new_risk = frisk_code-1
 				print(new_risk)
-			if new_risk == 0:
-				new_risk = 'No Risk'
-		
+				if new_risk == 0:
+					new_risk = 'No Risk'
+					st.markdown('**-Flood risk: **' + str(new_risk))
+					url1 = 'tablerisk.png'
+					image1 = Image.open(url1)
+					st.image(image1, caption='',use_column_width=True) 
+				else:
+					st.markdown('**-Flood risk: **' + str(new_risk))
+					url1 = 'tablerisk.png'
+					image1 = Image.open(url1)
+					st.image(image1, caption='',use_column_width=True) 
+
 		## TEST ##
 		## flood risk ==3 #15.2533,-61.3164
 		## flood risk ==4 #15.3393,-61.2603
 		## flood risk ==0 #15.3451,-61.3588
-		## LandslideNO + flood risk ==0 ## 15.3955,-61.2482
+		## Landslide NO risk + flood risk ==0 ## 15.4413,-61.3210
 		## LandslideXX + flood risk ==0 ## 15.4757,-61.2679
 
-		st.markdown('**-Flood risk: **' + str(new_risk))
-		url1 = 'tablerisk.png'
-		image1 = Image.open(url1)
-		st.image(image1, caption='',use_column_width=True) 
-
-# FloodRisk
-# No Risk
-# Low Risk (1)
-# Moderate Risk (2)
-# High Risk (3)
-# Very High (4)
-
-#Landslide Risk
-# No Risk
-# xx Risk (1)
 if __name__ == "__main__":
 	main()
 
